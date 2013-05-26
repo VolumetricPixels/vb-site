5e6e79480811de23e9c2be81480ff19d09a8a862
Ian Macalinao <ianmacalinao@gmail.com>
2013-05-26 17:01:38 -0500

    Add nodetime
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,8 @@
+if(process.env.NODETIME_ACCOUNT_KEY) {
+    require('nodetime').profile({
+        accountKey: process.env.NODETIME_ACCOUNT_KEY,
+        appName: 'Volumetric Bans'
+    });
+}
 require('coffee-script');
 module.exports = require('./lib/application');
